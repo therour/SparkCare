8bb9da5801592fa7bed119643c70c2a7d16695bf
Nizom Sidiq <nizomsidiq@gmail.com>
2018-12-09 23:41:56 +0700

    tes pertanyaan

--- app/src/main/java/id/ac/uii/fit/project/QuestionActivity.java
+++ app/src/main/java/id/ac/uii/fit/project/QuestionActivity.java
@@ -2,6 +2,8 @@ package id.ac.uii.fit.project;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -19,26 +21,34 @@ import id.ac.uii.fit.project.services.DatabaseService;
 public class QuestionActivity extends BaseActivity {
 
     private DatabaseReference db;
-    private List<Gejala> listGejala;
+    private List<Gejala> listGejala = new ArrayList<>();
+    private boolean isLoading;
+    private int currentQuestion = 0;
+    public static List<Gejala> answer = new ArrayList<>();
+    private TextView questionText;
+    private LinearLayout answerLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_activity);
+        answer.clear();
+        answerLinearLayout = findViewById(R.id.answerLinearLayout);
+        questionText = (TextView) findViewById(R.id.question_text);
 
-        listGejala = new ArrayList();
-        final TextView questionText = (TextView) findViewById(R.id.question_text);
+        showLoading(true);
+
         db = DatabaseService.gejala();
         db.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 listGejala.clear();
                 for (DataSnapshot gejalaSnapshot: dataSnapshot.getChildren()) {
-                    Gejala gejala = new Gejala(gejalaSnapshot.getKey(), gejalaSnapshot.child("nama").getValue().toString());
+                    Gejala gejala = Gejala.parse(gejalaSnapshot);
                     System.out.println(gejala.kode +": " + gejala.nama);
                     listGejala.add(gejala);
                 }
-                questionText.setText(listGejala.get(1).nama);
+                showLoading(false);
             }
 
             @Override
@@ -46,5 +56,47 @@ public class QuestionActivity extends BaseActivity {
 
             }
         });
+    }
+
+    public void actionYes(View view) {
+        if (! isFinishedAction()) {
+            showLoading(true);
+            Gejala gejala = listGejala.get(currentQuestion);
+            if (! answer.contains(gejala)) {
+                answer.add(gejala);
+            }
+            showLoading(false);
+        }
+    }
+
+    public void actionNo(View view) {
+        if (! isFinishedAction()) {
+            showLoading(true);
+            showLoading(false);
+        }
+    }
+
+    private void showLoading(Boolean isLoading) {
+        disableAction(isLoading, "Loading...");
+    }
+
+    private void disableAction(Boolean hideYesOrNo, String text) {
+        if (hideYesOrNo) {
+            questionText.setText(text);
+            answerLinearLayout.setVisibility(View.INVISIBLE);
+        } else {
+            questionText.setText(listGejala.get(currentQuestion).nama);
+            answerLinearLayout.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private boolean isFinishedAction() {
+        if (listGejala.size() <= ++currentQuestion) {
+            disableAction(true, "SELESAI");
+            finish();
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- app/src/main/java/id/ac/uii/fit/project/models/Gejala.java
+++ app/src/main/java/id/ac/uii/fit/project/models/Gejala.java
@@ -1,10 +1,23 @@
 package id.ac.uii.fit.project.models;
 
-import com.google.firebase.database.Exclude;
-import com.google.firebase.database.IgnoreExtraProperties;
+import android.support.annotation.NonNull;
 
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskCompletionSource;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 @IgnoreExtraProperties
 public class Gejala {
@@ -15,6 +28,45 @@ public class Gejala {
     public Gejala(String kode, String nama) {
         this.kode = kode;
         this.nama = nama;
+    }
+
+    @Exclude
+    public static List<Gejala> getAll() {
+        List<Gejala> listGejala = new ArrayList<>();
+        final TaskCompletionSource<DataSnapshot> tcs = new TaskCompletionSource<>();
+        DatabaseReference db = FirebaseDatabase.getInstance().getReference("gejala");
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                tcs.setResult(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                tcs.setException(databaseError.toException());
+            }
+        });
+
+        Task<DataSnapshot> t = tcs.getTask();
+        try {
+            Tasks.await(t);
+        } catch (ExecutionException | InterruptedException e) {
+            t = Tasks.forException(e);
+        }
+
+        if (! t.isSuccessful()) {
+            return listGejala;
+        }
+        DataSnapshot snapshot = t.getResult();
+        for (DataSnapshot gejalaSnapshot : snapshot.getChildren()) {
+            listGejala.add(parse(gejalaSnapshot));
+        }
+        return listGejala;
+    }
+
+    @Exclude
+    public static Gejala parse(DataSnapshot dataSnapshot) {
+        return new Gejala(dataSnapshot.getKey(), dataSnapshot.child("nama").getValue().toString());
     }
 
     @Exclude
