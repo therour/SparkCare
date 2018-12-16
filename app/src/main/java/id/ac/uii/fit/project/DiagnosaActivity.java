4bbe60e72f542d1f33b1224b72acb1915209d7c6
ariearfn <33951592+ariearfn@users.noreply.github.com>
2018-12-16 22:37:06 +0700

    logo new

--- app/src/main/java/id/ac/uii/fit/project/DiagnosaActivity.java
+++ app/src/main/java/id/ac/uii/fit/project/DiagnosaActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.Constraints;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
--- app/src/main/java/id/ac/uii/fit/project/HomeActivity.java
+++ app/src/main/java/id/ac/uii/fit/project/HomeActivity.java
@@ -2,6 +2,7 @@ package id.ac.uii.fit.project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class HomeActivity extends BaseActivity {
